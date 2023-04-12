486858287e357a3ddc0b7e26bf3982905c1d4bc2
Durian <dmsosa21@outlook.com>
2023-04-12 08:02:13 +0100

    Wir haben die Anzahl der Seiten dieses Seit gefunden!

--- scrapin.py
+++ scrapin.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup 
 import requests
+import re
 
 dinge = input("Welche Dinge mochtest du suchen?\n")
+dinge = dinge.capitalize()
 url = "https://www.newegg.com/p/pl?d=%s" %(dinge)
 
 uh = requests.get(url).text 
@@ -9,6 +11,18 @@ soup = BeautifulSoup(uh, 'html.parser')
 
 #Ermitteln der Seitenzahl dieses Seit
 
-a = soup.find("div", class_="list-tool-pagination").children
-for i in a:
-    print(i)
+a = soup.find(class_="list-tool-pagination").strong
+page_num = str(a).split("/")[-2].split(">")[-1][:-1]
+print(page_num)
+
+for p in range(1, 2):
+    url = f"https://www.newegg.com/p/pl?N=4131&d={dinge}&page={p}"
+    uh = requests.get(url).text
+    souphand = BeautifulSoup(uh, "html.parser")
+    #des Namens der Produkte erhalten
+
+    div = souphand.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    items = div.find_all(text=re.compile(dinge))
+    print(len(items))
+    for item in items:
+        print(item)
