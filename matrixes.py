05a366657abaf5135d8b0ba42b26c12be1f547e8
Durian <dmsosa21@outlook.com>
2023-04-12 18:47:24 +0100

    databasen jetz gemacht

--- matrixes.py
+++ matrixes.py
@@ -1,8 +1,12 @@
 import math
 d = {i: math.sqrt(i) for i in range(1, 101)}
 
-c = 0 
-for i in d.items():
-    if c < 4: 
-        print(i)
-    c += 1  
+c = "17.84"
+c = float(c)
+print(c)
+print(type(c))
+# c = 0 
+# for i in d.items():
+#     if c < 4: 
+#         print(i)
+#     c += 1  
--- scrapin.py
+++ scrapin.py
@@ -1,28 +1,65 @@
 from bs4 import BeautifulSoup 
+import sqlite3
 import requests
 import re
 
-dinge = input("Welche Dinge mochtest du suchen?\n")
-dinge = dinge.capitalize()
-url = "https://www.newegg.com/p/pl?d=%s" %(dinge)
+if __name__ == "__main__":
+    conn = sqlite3.connect("Prices.sqlite")
+    cur = conn.cursor()
 
-uh = requests.get(url).text 
-soup = BeautifulSoup(uh, 'html.parser')
+    cur.execute("""CREATE TABLE IF NOT EXISTS Products (
+        p_id INTEGER PRIMARY KEY UNIQUE, 
+        name TEXT UNIQUE)
+        """)
+    cur.execute("""CREATE TABLE IF NOT EXISTS Prices (
+        price_id INTEGER PRIMARY KEY UNIQUE, 
+        p_id INTEGER,
+        qty REAL)""")
+    
+    dinge = input("Welche Dinge mochtest du suchen?\n")
+    dinge = dinge.capitalize()
+    url = "https://www.newegg.com/p/pl?d=%s" %(dinge)
 
-#Ermitteln der Seitenzahl dieses Seit
+    uh = requests.get(url).text 
+    soup = BeautifulSoup(uh, 'html.parser')
 
-a = soup.find(class_="list-tool-pagination").strong
-page_num = str(a).split("/")[-2].split(">")[-1][:-1]
-print(page_num)
+    #Ermitteln der Seitenzahl dieses Seit
 
-for p in range(1, 2):
-    url = f"https://www.newegg.com/p/pl?N=4131&d={dinge}&page={p}"
-    uh = requests.get(url).text
-    souphand = BeautifulSoup(uh, "html.parser")
-    #des Namens der Produkte erhalten
+    a = soup.find(class_="list-tool-pagination").strong
+    page_num = str(a).split("/")[-2].split(">")[-1][:-1]
+    print(page_num)
 
-    div = souphand.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
-    items = div.find_all(text=re.compile(dinge))
-    print(len(items))
-    for item in items:
-        print(item)
+    for p in range(1, 2):
+        url = f"https://www.newegg.com/p/pl?N=4131&d={dinge}&page={p}"
+        uh = requests.get(url).text
+        souphand = BeautifulSoup(uh, "html.parser")
+        #des Namens der Produkte erhalten
+
+        items = souphand.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell").find_all(text=re.compile(dinge))
+        price_box = souphand.find(class_="price-current")
+        zahlpart = price_box.find_all('strong')
+        dotpart = price_box.find_all('sup')
+
+        print(len(items))
+        for item in items:
+            parent = item.parent
+            if parent.name != "a":
+                continue
+
+            link = parent['href']
+            price_container = item.find_parent(class_="item-container")
+            zahl_price = price_container.find(class_="price-current").strong.string
+            dot = price_container.find(class_="price-current").sup.string
+            price = float(zahl_price+dot)
+            
+            cur.execute('''INSERT OR IGNORE INTO Products (name) VALUES (?)''', (item,))
+            cur.execute('''SELECT p_id FROM Products WHERE name = (?) LIMIT 1''', (item, ))
+            row = cur.fetchone()
+            product_id = row[0]
+            cur.execute('''INSERT OR IGNORE INTO Prices (qty, p_id) VALUES (?, ?)''', (price, product_id))
+    
+    cur.execute('UPDATE Products SET p_id = 0, name = NULL')
+    cur.execute('UPDATE Prices SET price_id = 0, p_id = 0, name = NULL')
+    cur.close()
+    conn.commit()
+    
