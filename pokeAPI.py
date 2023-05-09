9ea40019be03fc1e4937d0d897b1f4dafb05b64c
Durian <dmsosa21@outlook.com>
2023-05-09 16:13:11 +0100

    Weitere Funktionalitaten zu unserem Programm hinzugefugt

--- pokeAPI.py
+++ pokeAPI.py
@@ -1,5 +1,6 @@
 def run():
     import urllib.request, urllib.parse, urllib.error
+    import math, random #math and random are just imported to pick an ability for our Pokemon randomly
     import sqlite3
     import requests
     import json
@@ -24,39 +25,58 @@ def run():
         pokemon = input('What Pokemon would you like to find? (insert Pokemon''s name or id!)')
         if len(pokemon) >= 3:
             break
+
     try:
         url = serviceurl.format(name=pokemon)
         uh = requests.get(url)
 
         suggestions = dict()
-        for n in range(1000):
+        for n in range(1, 3):
             pkmn = requests.get(serviceurl.format(name=n))
             pkdata = json.loads(pkmn.text)
             suggestName = pkdata['name']
+            print(suggestName)
             APid = pkdata['id']
-            for types in pkdata['types']
+            type = ""
+            ability = ""
+            hidden = None
+            for types in pkdata['types']:
+                if len(type) > 1:
+                    type += ", "
+                type += types['type']['name']
+            for ability in pkdata['abilities']:
+                x = math.floor(random.random()*len(pkdata['abilities']))
+                ability = pkdata['abilities'][x]['ability']['name']
+                if not pkdata['abilities'][x]['is_hidden']:
+                    hidden = 0
+                else:
+                    hidden = 1
+            print(ability, hidden)
+
             
+             
+
 
             cur.execute('''INSERT OR IGNORE INTO Pokemon (name, type, ability, hidden, APId)''')
-        #     commonWords = 0
-        #     for i in range(len(str(pokemon))):
-        #         if pokemon[i] == suggestName[i]:
-        #             commonWords += 1
-        #     if commonWords > 2:
-        #         suggestions[suggestName] = commonWords
-        # suggestList = list()
-        # for k,v in suggestions.items():
-        #     suggestList.append((v,k))
-        # print(suggestList)
-        # suggestList = sorted(suggestList, key=lambda x:x[0], reverse=True)
-        # print('Perhaps you wanted to search for: "'+suggestList[0][1]+'"?')
+            commonWords = 0
+            for i in range(len(str(pokemon))):
+                if pokemon[i] == suggestName[i]:
+                    commonWords += 1
+            if commonWords > 2:
+                suggestions[suggestName] = commonWords
+        suggestList = list()
+        for k,v in suggestions.items():
+            suggestList.append((v,k))
+        print(suggestList)
+        suggestList = sorted(suggestList, key=lambda x:x[0], reverse=True)
+        print('Perhaps you wanted to search for: "'+suggestList[0][1]+'"?')
         if uh.text == "Not Found":
             print("Not Found Error!")
             quit()
         
         print(uh.headers['Date'], "Content Length:", uh.headers['Content-Length'])
         
-        #trying to solve typo errors
+        # #trying to solve typo errors
 
 
                 
