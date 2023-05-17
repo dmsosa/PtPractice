614241c4afa7f92c5d4f0cddb91550574b022e5c
Durian <dmsosa21@outlook.com>
2023-05-17 19:15:57 +0100

    Erledigte Fachigskeiten, Erstellen von Beziehungen in unserer Datenbank

--- PokemonProject/fillPokeAPI.py
+++ PokemonProject/fillPokeAPI.py
@@ -67,13 +67,7 @@ def run():
             pass
     
     def fillEvos():
-        print("a")
-        # for n in range (1, 100+1):
-        #     f = requests.get(f'https://pokeapi.co/api/v2/evolution-chain/{n}/')
-        #     js = json.loads(f.text)
-        #     name = js['chain']['species']['name']
-        #     if name == "eevee":
-        #         print("We caught eevee!", js['id'])
+
 
         conn = sqlite3.connect('pokedex.sqlite')
         cur = conn.cursor()
@@ -139,20 +133,29 @@ def run():
 
     def fillAbs():
 
+
         serviceurl = 'https://pokeapi.co/api/v2/ability/{}'
         conn = sqlite3.connect('pokedex.sqlite')
         cur = conn.cursor()
 
+        #Checking if the table is already filled with data
+        cur.execute('SELECT * FROM Abilities')
+        if len(cur.fetchall()) > 1:
+            print("Table already filled up!")
+            return
+            
         cur.execute('''CREATE TABLE IF NOT EXISTS Abilities (
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
         name TEXT NOT NULL UNIQUE,
         effect TEXT,
         de_name TEXT NOT NULL UNIQUE,
         pokemon_with INTEGER,
-        pokemon_hidden INTEGER
-        )''')
+        pokemon_hidden INTEGER,
+        pokeshare TEXT
+        );''')
 
         while True:
+            auto = False
             many = input("How many abilities do you want to look for?\n")
             try: 
                 many = int(many)
@@ -161,57 +164,75 @@ def run():
             if many == 0:
                 print("You think you are funny, right?\n")
                 quit()
+            elif many > 10:
+                auto = True
+                automatic = input("those are a few, do you want to fill up automaticly?")
+                while automatic.lower() != "no" and automatic.lower() != "yes":
+                    automatic = input("Excuse me, do you want to fill up the data automaticly?")
+                    if automatic.lower() == "yes":
+                        auto = True
+                        break
+                    else:
+                        break
+                break
             else:
                 break
+ 
+                
 
         while many > 0:
-            answer = ""
-            ab = input("Insert ability name, bitte!\n")
-            many -= 1
-            try:
-                uh = requests.get(serviceurl.format(ab))
-                js = json.loads(uh.text)
-                if uh.text.lower() == 'not found' or (len(ab) < 1):
-                    while True:
-                        answer = input("That ability was not found, do you want to search one randomly?\n")
-                        if len(answer) < 1 or answer.lower() == 'no':
-                            print("Okay!")
-                            break
-                        if answer.lower() == 'yes':
-                            break
-                
-            except Exception as err:
-                print(err)
-                continue
-            
-            if answer == "yes":
+            if auto:
                 cur.execute('SELECT ability FROM Pokemon ORDER BY RANDOM() LIMIT 1')
                 ab = cur.fetchone()[0]
-                cur.execute('SELECT name FROM Abilities WHERE name = (?)',(ab,))
-                try:
-                    if len(cur.fetchone()[0]):
-                        print("That ability was already retrieved")
-                        continue
-                except TypeError:
-                    pass
-
+            else:
+                answer = ""
+                ab = input("Insert ability name, bitte!\n")
+                many -= 1
                 try:
                     uh = requests.get(serviceurl.format(ab))
+                    if uh.text.lower() == 'not found' or (len(ab) < 1) or (uh.text is None) < 0:
+                        while True:
+                            answer = input("That ability was not found, do you want to search one randomly?\n")
+                            if len(answer) < 1 or answer.lower() == 'no':
+                                print("Okay!")
+                                break
+                            if answer.lower() == 'yes':
+                                break
+                    
                 except Exception as err:
                     print(err)
-                    many -= 1
                     continue
-                many -= 1
+                
+                if answer == "yes":
+                    cur.execute('SELECT ability FROM Pokemon ORDER BY RANDOM() LIMIT 1')
+                    ab = cur.fetchone()[0]
+                    cur.execute('SELECT name FROM Abilities WHERE name = (?)',(ab,))
+                    try:
+                        if len(cur.fetchone()[0]) > 1:
+                            print("That ability was already retrieved")
+                            many -= 1
+                            continue
+                    except TypeError:
+                        pass
 
-                js = json.loads(uh.text)
             try:
+                uh = requests.get(serviceurl.format(ab))
+                many -= 1
+                js = json.loads(uh.text)
+            except Exception as err:
+                print(err)
+                many -= 1
+                continue
+
+            try:
+                de_effect = ''
                 for name in js['names']:
                     if name['language']['name'] == "de":
                         de_name = name['name']
                 for effect in js['effect_entries']:
                     if effect['language']['name'] == "de":
                         de_effect = effect['effect']
-                print(de_name, de_effect)
+
                 poke_with = dict()
                 for poke in js['pokemon']:
                     if not poke['is_hidden']:
@@ -221,17 +242,34 @@ def run():
                     poke_name = poke['pokemon']['name']
                     poke_with[poke_name] = hid
 
-                    poke_hid = 0
-                    poke_no_hid = 0
+                poke_hid = 0
+                poke_no_hid = 0
+                poke_with_ids = list()
 
-                for v in poke_with.values():
+                for k,v in poke_with.items():
                     if v == 1:
                         poke_hid += 1
                     else: 
                         poke_no_hid += 1
-            
-                cur.execute('''INSERT OR IGNORE INTO Abilities (name, effect, de_name, pokemon_with, pokemon_hidden)
-                VALUES (?, ?, ?, ?, ?)''', (ab, de_effect, de_name, poke_no_hid, poke_hid))
+
+
+                    cur.execute('SELECT id FROM Pokemon WHERE name = (?)',(k,))
+                    try:
+                        pokeid = cur.fetchone()[0]
+                        poke_with_ids.append(pokeid)
+                    except:
+                        continue
+
+                pokeshare_id = ""
+                c = 0
+                for id in poke_with_ids:
+                    if c > 1:
+                        pokeshare_id += " "+str(id)
+                    else:
+                        pokeshare_id += str(id)
+                    c += 1
+                cur.execute('''INSERT OR IGNORE INTO Abilities (name, effect, de_name, pokemon_with, pokemon_hidden, pokeshare)
+                VALUES (?, ?, ?, ?, ?, ?)''', (ab, de_effect, de_name, poke_no_hid, poke_hid, pokeshare_id))
                 print("new ability added to our pokedex")
             except KeyError:
                 pass
@@ -242,9 +280,91 @@ def run():
         cur.close()
         return
 
-    todo = input("Which Data do you want to fill up?")
-    if len(todo) < 1:
-        fillAbs()
+    def fillGames():
+        conn = sqlite3.connect('pokedex.sqlite')
+        cur = conn.cursor()
+
+        #Checking if the table is already filled with data
+        cur.execute('SELECT * FROM Games')
+        if len(cur.fetchall()) > 1:
+            print("Table already filled up!")
+            return
+        
+        cur.execute('''CREATE TABLE IF NOT EXISTS Games (
+        pokemon_id INTEGER NOT NULL UNIQUE,
+        games TEXT,
+        gen TEXT NOT NULL
+        )''')
+
+        while True:
+            how_many = input("Start the filling!\n")
+            
+            try:
+                how_many = int(how_many)
+                if how_many == 0:
+                    print("0 times! are you serious?")
+                    continue
+                break
+            except:
+                continue
+
+        while how_many > 0:
+            cur.execute('''SELECT APId FROM Pokemon ORDER BY RANDOM() LIMIT 1''')
+            id = cur.fetchone()[0]
+            url = "https://pokeapi.co/api/v2/pokemon/{poke}/"
+            uh = requests.get(url.format(poke=id))
+            try:
+                js = json.loads(uh.text)
+            except Exception as err:
+                print(err)
+                how_many -= 1
+                continue
+
+            game_names = list()
+            games = ""  
+            for index in js['game_indices']:
+                gname = index['version']['name']
+                game_names.append(gname)
+            
+            c = 0
+            for g in game_names:
+                if c > 0:
+                    games += " "+g
+                c += 1
+            url = "https://pokeapi.co/api/v2/pokemon-species/{poke}/"
+            uh = requests.get(url.format(poke=id))
+            try:
+                js = json.loads(uh.text)
+            except Exception as err:
+                print(err)
+                how_many -= 1
+                continue
+            gen = js['generation']['name']
+
+            cur.execute('''INSERT OR IGNORE INTO Games (pokemon_id, games, gen) VALUES(?, ?, ?)''', (id, games, gen))
+            print("new pokemon added!")
+            how_many -= 1
+        conn.commit()
+        cur.close()
+
+    while True:
+        todo = input("Which Data do you want to fill up?\n")
+        if len(todo) < 1:
+            continue
+        if todo.lower() == "pokemon":
+            fillPokemon()
+            break
+        elif todo.lower() == "evos":
+            fillEvos()
+            break
+        elif todo.lower() == "abs":
+            fillAbs()
+            break
+        elif todo.lower() == "games":
+            fillGames()
+            break
+        else:
+            continue
         
 
 if __name__ == '__main__':
--- PokemonProject/searchPokeAPI.py
+++ PokemonProject/searchPokeAPI.py
@@ -1,91 +1,86 @@
 def run():
     import sqlite3
-    import requests
 
     conn = sqlite3.connect('pokedex.sqlite')
     cur = conn.cursor()
-    serviceurl = "https://pokeapi.co/api/v2/pokemon/{name}/"
+    integer = False
     while True:
         pokemon = input('What Pokemon would you like to find? (insert Pokemon''s name or id!)\n')
-        if len(pokemon) >= 3:
+        if len(pokemon) < 1:
+            continue
+        try: 
+            pokemon = int(pokemon)
+            integer = True
+            break
+        except:
+            pokemon = pokemon.lower()
+            if len(pokemon) < 3:
+                continue
             break
 
-    try:
-        url = serviceurl.format(name=pokemon)
-        uh = requests.get(url)
-        if uh.text == "Not Found":
-            try:
-                suggestions = dict()
-                sorted_suggestion = list()
-                cur.execute("SELECT name FROM Pokemon")
-                pokemon = pokemon.lower()
-                for poke in cur:
-                    name = poke[0]
-                    commonLetter = 0
-                    if len(name) < len(pokemon):
-                        continue
-                    if not (pokemon[:3] in name):
-                        continue
-                    # if not name.startswith(pokemon[:3]):
-                    #     continue
-                    for letter in range(len(name)):
-                        try:
-                            if name[letter+name.index(pokemon[:3])] == pokemon[letter]:
-                                commonLetter += 1
-                        except IndexError:
-                            break
-                    suggestions[name] = commonLetter
+    if integer:
+        print("i")
+        cur.execute('''SELECT id, name, ability FROM Pokemon WHERE APId = (?)''',(pokemon,))
+    else:
+        cur.execute('''SELECT id, name, ability FROM Pokemon WHERE name = (?)''',(pokemon,))
 
-                for k,v in suggestions.items():
-                    sorted_suggestion.append((v,k))
-                sorted_suggestion = sorted(sorted_suggestion, reverse=True)
-                while True:
-                    typo = input("perhaps you wanted to type: '"+sorted_suggestion[0][1]+"'?\n")
-                    if len(typo) < 1 or typo.lower() == "no":
-                        print("this pokemon is not in our pokedex!\n")
-                        quit()
-                    if typo.lower() == "yes":
-                        pokemon = sorted_suggestion[0][1]
-                        print("searching "+pokemon+" data...")
-                        quit()
-                        # url = serviceurl.format(name=pokemon)
-                        # uh = requests.get(url)
-            except IndexError:
-                print("this pokemon is not in our pokedex!\n")
-                quit()
-            except Exception as err:
-                print(err)
-                quit()
+    try: 
+        poke = cur.fetchone()[0]
+    except TypeError:
+        try:
+            suggestions = dict()
+            sorted_suggestion = list()
+            cur.execute("SELECT name FROM Pokemon")
+            for poke in cur:
+                name = poke[0]
+                commonLetter = 0
+                if len(name) < len(pokemon):
+                    continue
+                if not (pokemon[:3] in name):
+                    continue
+                # if not name.startswith(pokemon[:3]):
+                #     continue
+                for letter in range(len(name)):
+                    try:
+                        if name[letter+name.index(pokemon[:3])] == pokemon[letter]:
+                            commonLetter += 1
+                    except IndexError:
+                        break
+                suggestions[name] = commonLetter
+
+            for k,v in suggestions.items():
+                sorted_suggestion.append((v,k))
+            sorted_suggestion = sorted(sorted_suggestion, reverse=True)
+            while True:
+                typo = input("perhaps you wanted to type: '"+sorted_suggestion[0][1]+"'?\n")
+                if len(typo) < 1 or typo.lower() == "no":
+                    print("this pokemon is not in our pokedex!\n")
+                    return
+                if typo.lower() == "yes":
+                    pokemon = sorted_suggestion[0][1]
+        except IndexError:
+            print("this pokemon is not in our pokedex!\n")
+            quit()
+        except Exception as err:
+            print(err)
+            quit()
+    if cur.fetchone() is None:
+        cur.execute('''SELECT id, name, ability FROM Pokemon WHERE name = (?)''', (pokemon,))
+    pkid = cur.fetchone()[0]
+    pkname = cur.fetchone()[1]
+    pkab = cur.fetchone()[2]
+    cur.execute('''SELECT Pokemon.id, Pokemon.name, Pokemon.ability, Abilities.effect, 
+    Abilities.pokeshare, Games.games, Games.gen FROM Pokemon JOIN Abilities JOIN Games ON 
+    Abilities.name = (?) AND Games.pokemon_id = (?)''',(pkab, pkid))
+
         
 
             
              
 
 
-        #     cur.execute('''INSERT OR IGNORE INTO Pokemon (name, type, ability, hidden, APId)''')
-        #     commonWords = 0
-        #     for i in range(len(str(pokemon))):
-        #         if pokemon[i] == suggestName[i]:
-        #             commonWords += 1
-        #     if commonWords > 2:
-        #         suggestions[suggestName] = commonWords
-        # suggestList = list()
-        # for k,v in suggestions.items():
-        #     suggestList.append((v,k))
-        # print(suggestList)
-        # suggestList = sorted(suggestList, key=lambda x:x[0], reverse=True)
-        # print('Perhaps you wanted to search for: "'+suggestList[0][1]+'"?')
+    
 
-        
-        # print(uh.headers['Date'], "Content Length:", uh.headers['Content-Length'])
-        
-        # #trying to solve typo errors
-
-
-                
-                
-    except:
-        pass
 
 if __name__ == '__main__':
     run()
