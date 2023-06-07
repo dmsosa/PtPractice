f2e33466dbb4eea284a19c692a60512b1c9bb643
Durian <dmsosa21@outlook.com>
2023-06-07 09:11:05 +0100

    gangelmann erfolg 03

--- hangman/gangelmannchen.py
+++ hangman/gangelmannchen.py
@@ -1,27 +1,35 @@
-import math, random, os
+import math, random, os, time
 def fill(response, indexes, man):
     new_response = list()
     rat = list(indexes.keys())[0]
     index = list(indexes.values())[0]
-
+    bars = "- "
     for i in response:
         new_response.append(i)
     for i in range(len(response)):
+        bars += "-"
         if i in index:
             new_response[i] = rat
     response = ""
     for i in range(len(new_response)):
         response += new_response[i]
     os.system('cls')
-    print(response)
+    print("   "+response+"\n"+bars+" -")
     print(man)
     print('Gut gemacht!')
+    return response
 
     
 def draw(response, man, fails):
-    print(response)
-    if fails == 1:
-        man += ""
+    os.system('cls')
+    print("   "+response+"\n")
+    model_3 = "  ____\n |    |\n |    O\n |   /|\\\n |   / \\\n | \n_|_\n"
+    man = model_3[0:(fails*4+1)]
+    for i in range(fails*4):
+        print(model_3[i], end="")
+    print("")
+    return man
+   
 def getwort():
     with open("./data.txt", "r") as f:
         worter = f.read().split("\n")
@@ -30,10 +38,7 @@ def getwort():
         return wort
 
 def arbeit(wort):
-    print(wort)
-    j = list()
     dic = {k:[i for i in range(len(wort)) if wort[i] == k] for k in wort}
-    print(dic)
     return dic
 
 def check(rat, dic):
@@ -41,20 +46,21 @@ def check(rat, dic):
     indexes = {}
     if rat in dic.keys():
         indexes[rat] = dic.get(rat)
-        return indexes
-    elif len(indexes) < 1:
-        return indexes
+    return indexes
 
 
 def start_game():
     os.system('cls')
     
+    gover = False
+    gwon = False
+    try_again = False
     wort = getwort()
     dic = arbeit(wort)
     response = ""
+    man = ""
     fails = 0
     guesses = list()
-    man = ""
     for i in range(len(wort)):
         response += "_"
     print(response)
@@ -72,12 +78,45 @@ def start_game():
             continue
         indexes = check(rat, dic)
         if len(indexes) > 0:
-            fill(response, indexes, man)
+            response = fill(response, indexes, man)
         else:
             print('Not found!')
             fails += 1
-            draw(response, man, fails)
-            continue
+            man = draw(response, man, fails)
+            if fails == 12:
+                print('//////////////////////\nRuhe in Frieden x_x')
+                break
+    if gover:
+        while True:
+            ans = input(" . . . (game over) . . .\nwant to try again?")
+            if ans.lower() == 'yes':
+                try_again = True
+                print('Das ist es!\nLos gehts!')
+                time.sleep(2)
+                os.system('cls')
+                break
+            elif ans.lower() == 'no':
+                print('Wir hoffen es, du noch einmail wiedersehen!')
+                time.sleep(2)
+                print('Tschuss!')
+                break
+    elif gwon:
+        while True:
+            ans = input("Du schafftet es!\nMochtest du noch einmal spielen?")
+            while True:
+                ans = input(" . . . (game over) . . .\nwant to try again?")
+                if ans.lower() == 'yes':
+                    try_again = True
+                    print('Das ist es!\nLos gehts!')
+                    time.sleep(2)
+                    os.system('cls')
+                    break
+                elif ans.lower() == 'no':
+                    print('Wir hoffen es, du noch einmail wiedersehen!')
+                    time.sleep(2)
+                    print('Tschuss!')
+                    break
+    return try_again
 
 
 
@@ -86,40 +125,9 @@ def start_game():
 
 
 def run():
-    dif = 48
-    man = ""
-    while dif > 0:
-        p1 = "  ____\n"
-        p2 = " |    |\n"
-        p3 = " |    O\n"
-        p4 = " |   /|\\\n"
-        p5 = " |   / \\\n"
-        p6 = " | \n"
-        p7 = "_|_\n"
-        total = p1+p2+p3+p4+p5+p6+p7
-        model_1 = "      \n | \n | \n | \n | \n | \n_|_\n"
-        model_2 = " ____\n |    |\n | \n | \n | \n | \n_|_\n"
-        model_3 = " ____\n |    |\n |    O\n |   /|\\\n |   / \\\n | \n_|_\n"
-        print(len(" |   / \\\n"))
-        print(model_1, model_2, model_3)
-        f = int(input('from'))
-        t = int(input('to'))
-        print(total[f:t])
-        # print(str(len(total))+"\n"+total)
-        # dif = len(total) - len(man) - 4
-        # steps = 48 - dif
-        # step = int(steps/4)
-
-        # man = ""
-        # for i in range(step):
-        #     if 0 <= i <= 2:
-        #         man += total[i]
-        #     man += total[i]
-
-        # if steps == 48:
-        #     print("Ruhe in Frieden x_x")
-        # print(man)
-        # e = input("step "+ str(int(steps/4))+" out of 12")
+    spiel = start_game()
+    while spiel:
+        spiel = start_game()
 
 
 if __name__ == '__main__':
