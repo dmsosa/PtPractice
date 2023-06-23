4441b954f81b28c2751729ffb82c8545c8f243e6
Durian <dmsosa21@outlook.com>
2023-06-23 16:55:45 +0100

    UP 03, Unsere motivationalen Absatzen fertig sein!

--- WebScrapping/Web/motivation.py
+++ WebScrapping/Web/motivation.py
@@ -5,42 +5,49 @@ import sys
 sys.stdin.reconfigure(encoding='utf-8')
 sys.stdout.reconfigure(encoding='utf-8')
 
+def run():
+    writephrases()
 
-conn = http.client.HTTPSConnection('www.riminitoday.it')
-conn.request(method="GET",url='/social/frasi-motivazionali-per-sentirsi-meglio.html')
-r = conn.getresponse()
-html = r.read().decode()
+    
+def writephrases():
+    conn = http.client.HTTPSConnection('www.riminitoday.it')
+    conn.request(method="GET",url='/social/frasi-motivazionali-per-sentirsi-meglio.html')
+    r = conn.getresponse()
+    html = r.read().decode()
 
-soup = BeautifulSoup(html, 'html.parser')
-h2 = soup.find_all('h2')[1:]
-headers = list()
-for h in h2:
-    headers.append(h.text)
-phrases = soup.find_all('p')
-phraseBook = dict()
-for p in phrases:
-    try:
-        sentence = re.search('[0-9]+(.*)', p.text).group().split('(')
-        phrase = sentence[0].strip('0123456789. ')
-        author = sentence[1][:-1].split(",")[0]
-        phraseBook[phrase] = author
-    except: continue
+    soup = BeautifulSoup(html, 'html.parser')
+    h2 = soup.find_all('h2')[1:]
+    headers = list()
+    for h in h2:
+        headers.append(h.text)
+    phrases = soup.find_all('p')
+    phraseBook = dict()
+    for p in phrases:
+        try:
+            sentence = re.search('[0-9]+(.*)', p.text).group().split('(')
+            phrase = sentence[0].strip('0123456789. ')
+            author = sentence[1][:-1].split(",")[0]
+            phraseBook[phrase] = author
+        except: continue
 
-with open('./phrases.xml', 'w',encoding='utf-8') as f:
+    with open('./phrases.xml', 'w',encoding='utf-8') as f:
 
-    i = 0
-    c = 0
+        i = 0
+        c = 0
 
-    f.write('<ul class="{}">\n'.format(headers[i]))
-    for k,v in phraseBook.items():
-        f.write('  <li>\n    <p>'+k+'</p>\n    <a>'+v+'</a>\n  </li>\n')
-        c += 1
-        if c % 10 == 0:
-            try:
-                i += 1
-                f.write('</ul>\n<ul class="{}">\n'.format(headers[i]))
-            except IndexError:
-                continue
-    f.write('</ul>')
+        f.write('<ul class="{}">\n'.format(headers[i]))
+        for k,v in phraseBook.items():
+            f.write('  <li>\n    <p>'+k+'</p>\n    <a>'+v+'</a>\n  </li>\n')
+            c += 1
+            if c % 10 == 0:
+                try:
+                    i += 1
+                    f.write('</ul>\n<ul class="{}">\n'.format(headers[i]))
+                except IndexError:
+                    continue
+        f.write('</ul>')
 
-print('File written!')
+    print('File written!')
+
+if __name__ == '__main__':
+    run()
--- WebScrapping/Web/phrases.py
+++ WebScrapping/Web/phrases.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import http.client
+import http.client, json, urllib.parse
 
 def set_quotes():
         with open('./phrases.xml', 'r', encoding='utf-8') as f:
@@ -13,12 +13,15 @@ def set_quotes():
                 quotes[quote] = author
             return quotes
 
-def ubersetzer(lang, text):
-    assert for lang in code type(lang) == str and len(lang) == 2, f'Ungultiger Landercode! "{lang}"'
-    text = text.encode()
+def ubersetzer(src, ziel, text):
+    assert (type(src) == str and type(ziel) == str) and (len(src) == 2 and len(ziel) == 2), f'Ungultiger Landercode! "{src}", "{ziel}"'
+
+    text = urllib.parse.quote(text)
+    src_lang = src.lower()
+    ziel_lang = ziel.lower()
     api_key = 'f26fbfc3c5msh0ed852bd7999cdbp14fd28jsnec2403b96d20'
     host = 'text-translator2.p.rapidapi.com'
-    payload = f'source_language=en&target_language={lang}&text={text}'
+    payload = f'source_language={src_lang}&target_language={ziel_lang}&text={text}'
     headers = {
          'X-RapidAPI-Key': api_key,
          'X-RapidAPI-Host': host,
@@ -28,16 +31,25 @@ def ubersetzer(lang, text):
     conn = http.client.HTTPSConnection(host)
     conn.request('POST', '/translate', payload, headers)
     response = conn.getresponse()
-    translation = response.read().decode('utf-8')
-    return translation
+    js = json.loads(response.read())
 
+    return js['data']['translatedText']
+
+def translator(iterable, translator=ubersetzer, lang='de'):
+    translated_phrases = dict()
+    for phrase, author in iterable.items():
+        translated = ubersetzer('it', lang, phrase)
+        translated_phrases[translated] = author
+    return translated_phrases
+         
+     
 def run():
     quotes = set_quotes()
-    source_lang = input('In welcher Sprache schreiben Sie?')
-    phrase = input('Geben Sie bitte ein Absatz und ein Landercode')
-    ziel_lang = input('In welche Sprache mochten Sie es ubersetzen?')
-    codes = [source_lang, ziel_lang]
-    print(ubersetzer(code, phrase))
+    ziel_lang = input('In welche Sprache mochten Sie die Absatzen ubersetzen?\n')
+    if len(ziel_lang) < 1: ziel_lang = 'de'
+    translated_quotes = translator(quotes, lang=ziel_lang)
+    for q,v in translated_quotes.items():
+        print(q,v)
 
 if __name__ == '__main__':
     run()
